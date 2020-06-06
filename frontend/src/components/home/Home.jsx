256b745c1726aa41c21f4a587203cb8e79886500
Inácio Ferraz <zoawbrc@gmail.com>
2020-06-06 09:12:32 -0300

    CRUD feito enquanto estudo REACT.js

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props => 
+    <Main icon='home' title='inicio' subtitle='Segundo projeto react'>
+        <div className="display-4">Bem vindo!!</div>
+        <hr/>
+        <p className="mb-0">
+            Sistema para exemplificar a construção de um cadastro desenvolvido em React!!
+        </p>
+    </Main>
